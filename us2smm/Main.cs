f1aa71ed959251b8e12c3adb3e2c5cfbe72da68f
darkedge <aba47c75@opayq.com>
2015-06-28 19:18:22 +0200

    Argument check

--- us2smm/Main.cs
+++ us2smm/Main.cs
@@ -5,10 +5,16 @@ using System.Text;
 
 namespace us2smm
 {
-    class Test
+    class Converter
     {
+        
+
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                
+            }
             Console.WriteLine("Hello World!");
         }
     }
