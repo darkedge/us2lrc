f53ff4e3f26ae29dc3bc3f13c9cdffde0394c2c2
darkedge <aba47c75@opayq.com>
2015-06-28 18:59:10 +0200

    Initial commit

---
+++ us2smm/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace us2smm
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
