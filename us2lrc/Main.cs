e2c691f848095f7d1963502d803d572e466f5028
darkedge <aba47c75@opayq.com>
2015-06-29 19:08:41 +0200

    Working directory/file creation

--- us2lrc/Main.cs
+++ us2lrc/Main.cs
@@ -12,19 +12,19 @@ namespace us2lrc
         public class Tags
         {
             // == Mandatory tags ==
-            public static const string Title = "#TITLE:";
-            public static const string Artist = "#ARTIST:";
-            public static const string Mp3 = "#MP3:";
-            public static const string Gap = "#GAP:";
-            public static const string Bpm = "#BPM:";
+            public const string Title = "#TITLE:";
+            public const string Artist = "#ARTIST:";
+            public const string Mp3 = "#MP3:";
+            public const string Gap = "#GAP:";
+            public const string Bpm = "#BPM:";
 
             // == Optional tags ==
-            public static const string Genre = "#GENRE:";
-            public static const string Edition = "#EDITION:";
-            public static const string Cover = "#COVER:";
-            public static const string Video = "#VIDEO:";
-            public static const string Background = "#BACKGROUND:";
-            public static const string Relative = "#RELATIVE:";
+            public const string Genre = "#GENRE:";
+            public const string Edition = "#EDITION:";
+            public const string Cover = "#COVER:";
+            public const string Video = "#VIDEO:";
+            public const string Background = "#BACKGROUND:";
+            public const string Relative = "#RELATIVE:";
         }
 
         // == Mandatory tags ==
@@ -58,21 +58,35 @@ namespace us2lrc
 
     class Converter
     {
-        void ConvertUSToTXT(string path, string outPath)
+        static void ConvertUSToTXT(string path, string outPath)
         {
+            Console.WriteLine("Converting file: " + path);
+
             // Read file
             using (StreamReader rdr = new StreamReader(path))
             {
                 string line;
                 while ((line = rdr.ReadLine()) != null)
                 {
-                    // use line here
+                    var matches = Regex.Match(line, "(?#<TAG>.*):(?<VALUE>.*)");
+                    if (matches.Success)
+                    {
+                        Console.WriteLine(matches.Groups["TAG"].Value);
+                        Console.WriteLine(matches.Groups["VALUE"].Value);
+                    }
                 }
             }
 
+            //FileStream file = new FileStream("highscores.txt", FileMode.Append, FileAccess.Write);
+
             // Create destination text file
-            using (StreamWriter file = new StreamWriter(@"C:\Users\Public\TestFolder\WriteLines2.txt"))
+            string fileWOExtension = Path.GetFileNameWithoutExtension(path);
+            string outFile = Path.Combine(outPath, fileWOExtension + ".lrc");
+            using (StreamWriter file = new StreamWriter(outFile))
             {
+                // Write file
+
+                /*
                 foreach (string line in lines)
                 {
                     // If the line doesn't contain the word 'Second', write the line to the file. 
@@ -81,6 +95,7 @@ namespace us2lrc
                         file.WriteLine(line);
                     }
                 }
+                 * */
             }
         }
 
@@ -105,7 +120,7 @@ namespace us2lrc
             Directory.CreateDirectory(outPath);
 
             // Put all txt files in root directory into array.
-            string[] inFiles = Directory.GetFiles(outPath, "*.txt"); // <-- Case-insensitive
+            string[] inFiles = Directory.GetFiles(inPath, "*.txt"); // <-- Case-insensitive
 
             foreach (string name in inFiles)
             {
